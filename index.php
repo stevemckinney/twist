ade7494946f42ba832e2012d8921f8ff7782ee61
Steve McKinney <stemckinney@mac.com>
2012-02-09 22:19:47 +0000

    Modals, notices, stuff

--- index.php
+++ index.php
@@ -94,7 +94,7 @@
 			
 			<fieldset>
 				<label for="email">Email</label>
-				<input type="email" id="email" class="form-text">
+				<input type="email" id="email" class="form-text error">
 			</fieldset>
 			
 			<fieldset>
@@ -133,6 +133,33 @@
 				<input type="submit" value="Submit">
 			</fieldset>
 		</form>							
+	</section>
+	
+	<section role="region">
+		<h1>Notices</h1>
+		
+		<p class="notice">Data saved!</p>
+		<p class="notice">Today 50% off only.</p>
+		<p class="notice">Please update your info immediately</p>
+		<p class="notice">You're due a payment in 3 days</p>
+	
+	</section>
+	
+	<section>
+		<h1>General elements</h1>
+		
+		<div class="overlay">
+			<div class="modal" id="modal">
+				<h3>Modal window</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<div class="button_group">
+					<a href="#modal" class="subtle">Remind me later</a>
+					<a href="#" class="subtle">Now</a>
+					<a href="#" class="subtle">Later</a>
+				</div>
+			</div>
+		</div>	
+		
 	</section>
 	
 </section>
