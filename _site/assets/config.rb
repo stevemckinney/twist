2db5ca4a58e0003056cac4c7f33699d1d2573490
Steve McKinney <steve@iamsteve.me>
2013-02-01 21:08:09 +0000

    comments and rewording

--- _site/assets/config.rb
+++ _site/assets/config.rb
@@ -1,6 +1,7 @@
 # Require any additional compass plugins here.
-require 'animate-sass'
-require 'compass-recipes'
+# require 'animate-sass'
+# require 'compass-recipes'
+require 'ceaser-easing'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
