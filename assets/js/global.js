0dc4644a082de55a53e653ff072c9a1ffadd2cd2
Steve McKinney <steve@iamsteve.me>
2017-09-14 10:56:44 +0100

    clean up old code and just add some basic stuff

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,59 +1,15 @@
-var $n = $('.navigation'),
-		$nt = $('.navigation-toggle'),
-		$container = $('.site'),
-		$h = $('.header'),
-		$b = $('body');
+const Site = (function Site() {
+  // Function placeholder
+  const doStuff = function doStuff() {
+    // Do stuff
+  }
 
-var navigation_size_class_names = 'navigation-small navigation-large',
-		navigation_state_class = 'navigation-inactive navigation-active';
-
-// Breakpoint $c
-enquire.register('screen and (min-width: 48em)', {
-	
-	match : function() {
-		// Remove small navigation styles, add large styles,
-		// append to header and remove state classes
-		$n.toggleClass(navigation_size_class_names)
-			.removeClass(navigation_state_class)
-			.appendTo($h);
-		$container.removeClass('site-active');
-	},
-	
-	unmatch : function() {
-		$n.toggleClass(navigation_size_class_names)
-			.prependTo($b);
-	},
-
-	setup : function() {},
-
-	deferSetup : true,
-
-	// OPTIONAL
-	// If supplied, triggered when handler is unregistered.
-	// Place cleanup code here
-	destroy : function() {}
+  return {
+    init() {
+      // Initialise things
+      doStuff();
+    }
+  }
 });
 
-// Navigation toggle
-// ---------------------------
-$nt.on('click touchstart', function(e) {
-	e.preventDefault();
-	
-	$n.toggleClass(navigation_state_class);
-	
-	// Toggle active state
-	$nt.toggleClass('navigation-toggle-active');
-	
-	// Slide the container over
-	$container.toggleClass('site-active');
-	
-	// Apply overflow: hidden to body,
-	// otherwise we get a nasty empty scroll area,
-	// certain this is a bug
-	$b.toggleClass('overflow-hidden');
-});
-
-// Add close buttons to notices
-// ---------------------------
-var notice = new Notice();
-notice.close();
+Site.init();
